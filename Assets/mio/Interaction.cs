baf6b8421381b282d5f215e2dd5ec2593be975d5
Dariibu <davidblancopozo@gmail.com>
2022-12-12 23:36:40 +0100

    Added particles and sounds for click

--- Assets/mio/Interaction.cs
+++ Assets/mio/Interaction.cs
@@ -6,6 +6,8 @@ public class Interaction : MonoBehaviour
 {
     [SerializeField] Manager ScriptManager;
     Sprite startSprite;
+    public ParticleSystem ps;
+    public AudioSource au;
     private void Start()
     {
         startSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
@@ -15,6 +17,8 @@ public class Interaction : MonoBehaviour
         if (gameObject.GetComponent<SpriteRenderer>().sprite == startSprite && !ScriptManager.GetComponent<Manager>().GameOver)
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = ScriptManager.GetComponent<Manager>().Paint(name);
+            Instantiate(ps, transform);
+            au.Play();
         }
  
     }
